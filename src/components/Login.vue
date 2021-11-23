dcabd430394beea4ba3b99f670b9be9d1cf0a612
zhouyi <kkakoz@163.com>
2021-11-23 14:55:44 +0800

    add login vue file

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <div>username</div>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+</style>
